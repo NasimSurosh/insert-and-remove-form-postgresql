17fd15787d70c7d065cfda7fea23eb80028bb1f4
NasimSurosh <137775861+NasimSurosh@users.noreply.github.com>
2024-02-09 16:06:43 +0100

    Update DBConnection.java
    removed name password and url

--- src/com/files/postgresql/DBConnection.java
+++ src/com/files/postgresql/DBConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DBConnection {
   
-  private static final String user = "postgres";
-  private static final String pwd = "Nasim646016";
-  private static final String url = "jdbc:postgresql://localhost:5432/test";
+  private static final String user = "  ";
+  private static final String pwd = "  ";
+  private static final String url = "  ";
   
   public static Connection getConnection() throws SQLException {
     Connection connection = DriverManager.getConnection(url, user, pwd);
